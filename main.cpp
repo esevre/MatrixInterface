01f4e113fae9a8b8fbbb9a473fea8d5d288c8fb9
esevre <esevre@gmail.com>
2020-08-28 16:01:23 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Goal is to create a matrix interface
+//   to simplify working with matrix
+//   in a way that is consistent with
+//   C++ best practices
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
