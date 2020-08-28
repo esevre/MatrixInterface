d0d415bf0b2129fdf2ab9937e100dbc8ab009387
esevre <esevre@gmail.com>
2020-08-28 16:12:56 +0900

    MatrixInterface file added with basic ideas of implementation

---
+++ MatrixInterface.hpp
@@ -0,0 +1,14 @@
+//
+// Created by esevre on 8/28/2020.
+//
+//  File to manage MatrixInterface
+//
+//  There are a variety of features we want access to
+//    to deal with a matrix in a natural way
+//      - interface connects to data object with contiguous data
+//          should use data[index] to access elements
+//      - access by (row,col) index pair
+//
+#pragma once
+
+
